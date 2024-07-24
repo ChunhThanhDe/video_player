949d586eba4b465e634c2ea0d9d5d0e3865a5fe7
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-07-24 08:29:05 +0700

    :rocket: Initial commit

---
+++ app/src/main/java/com/chunhthanhde/example/video_player/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.chunhthanhde.example.video_player
+
+import android.net.Uri
+import android.net.Uri.parse
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.chunhthanhde.example.video_player.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //Find the VideoView class by its id
+        val videoView = findViewById<VideoView>(binding.testView.id)
+        //Creating MediaController
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        //specify the location of media file
+        val uri: Uri = parse(
+            "android.resource://" + packageName
+                    + "/raw/raw"
+        )
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
